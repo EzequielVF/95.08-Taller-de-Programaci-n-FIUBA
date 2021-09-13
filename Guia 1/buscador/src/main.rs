ab9977b534ec57f31050a3e6f0cfc799930b2d21
EzequielVF <57638464+EzequielVF@users.noreply.github.com>
2021-09-13 16:28:01 -0300

    -Buscador -> 40%
    - Tengo la verficacion de stop-words
    - Construyo el indice
    
    Falta
    -> Hacer las busquedas
    -> eliminar las simbolos de puntuacion

--- Guia 1/buscador/src/main.rs
+++ Guia 1/buscador/src/main.rs
@@ -1,32 +1,83 @@
-use std::io::{Read, Result, stdin};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
 
-#[derive(Debug)]
-struct Token<'a> {
-    s: &'a str,
-    start_index: usize,
-}
-
-fn tokenize<'a>(code: &'a str) -> impl Iterator<Item=Token<'a>> {
-    let mut next_token = 0;
-    let code_length = code.len();
-    code.char_indices()
-        .filter_map(move |(index, c)|
-            if c.is_whitespace() {
-                let start_index = next_token;
-                next_token = index + 1;
-                Some(Token { s: &code[start_index..index], start_index })
-            } else if index == code_length - 1 {
-                Some(Token { s: &code[next_token..index + 1], start_index: next_token })
-            } else {
-                None
-            })
-}
-
-fn main() -> Result<()> {
-    let mut buffer = "Holaaa@ bebe!!".to_string();
-
-    for token in tokenize(buffer.as_str()) {
-        print!("{:?} ", token);
+fn leer_y_almacenar_stopwords() -> HashMap<String, i32>{
+    let mut hash: HashMap<String, i32> = HashMap::new();
+    let filename = "src/stopwords.txt";
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        hash.insert(line.unwrap(), 1);
     }
-    Ok(())
+    hash
+}
+
+fn es_stopword(stop_words: &HashMap<String, i32>, palabra :&str) -> bool{
+    if stop_words.contains_key(palabra) {
+        true
+    } else {
+        false
+    }
+}
+
+fn recorrer_archivo_y_almacenar(indice: &mut HashMap<String, HashMap<String, i32>>, stop_words: &HashMap<String, i32>, filename: &str, id: &str) {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let oracion: String = line.unwrap().to_lowercase().into();
+        let chunks: Vec<_> = oracion.split_whitespace().collect();
+        for palabra in chunks {
+            if !es_stopword(stop_words, palabra) { // Si no es una stopword lo agrego
+                if indice.contains_key(&palabra.to_string()) { //Si la palabra ya esta en el indice
+                    let mut aux = indice.get(&palabra.to_string()).unwrap().clone();
+                    if aux.contains_key(&id.to_string()) { //Ya aparecio esta palabra en este documento antes
+                        let valor = aux.get(&id.to_string()).unwrap();
+                        let valor = valor + 1;
+                        aux.insert(id.to_string(), valor); // Actualizo el valor de apariciones en el hash
+                        indice.insert(palabra.to_string(), aux.clone()); //Actualizo el hash asociado a esa clave
+                    } else { //La palabra no aparecio previamente en este documento
+                        aux.insert(id.to_string(), 1);
+                        indice.insert(palabra.to_string(), aux.clone());
+                    }
+                } else { // Si no estaba todavia en el indice
+                    let mut hash_aux: HashMap<String, i32> = HashMap::new();
+                    hash_aux.insert(id.to_string(), 1);
+                    indice.insert(palabra.to_string(), hash_aux);
+                }
+            }
+        }
+    }
+}
+
+fn leer_y_llenar_indice(stop_words :&HashMap<String, i32>) -> HashMap<String, HashMap<String, i32>>{
+    let mut indice: HashMap<String, HashMap<String, i32>> = HashMap::new();
+    recorrer_archivo_y_almacenar(&mut indice, stop_words,"src/texto1.txt", "1");
+    recorrer_archivo_y_almacenar(&mut indice, stop_words,"src/texto2.txt", "2");
+    recorrer_archivo_y_almacenar(&mut indice, stop_words,"src/texto3.txt", "3");
+    indice
+}
+
+fn leer_hash(hash: &HashMap<String, i32>) {
+    for (key, value) in hash.iter() {
+        println!("{} - {}", key, value);
+    }
+}
+
+fn recorrer_indice(indice: &HashMap<String, HashMap<String, i32>>) {
+    for (key, hash) in indice.iter() {
+        println!("La palabra:{}", key);
+        println!("Aparece en:");
+        for (id, cantidad) in hash.iter() {
+            println!("En el Documento -> {},  -> {} veces.", id, cantidad);
+            println!();
+        }
+    }
+}
+
+fn main() {
+    let stop_words = leer_y_almacenar_stopwords(); //Me guardo las palabras que no deberia almacenar
+    //leer_hash(&stop_words); //Muestra la lista de stop-words
+    let indice :HashMap<String, HashMap<String, i32>> = leer_y_llenar_indice(&stop_words);
+    recorrer_indice(&indice);
 }
